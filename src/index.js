1437df64e2c6a48fda20ae225bbc59606a5f1a40
dan <dis.131998@gmail.com>
2025-10-06 18:24:00 +0300

    add: basic styles

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { getWeather } from "./weather.js";
 const submitBtn = document.querySelector("#submit")
 const input = document.querySelector("input[name='location']")
 
-submitBtn.addEventListener("click", e => {
-    e.preventDefault()
+// submitBtn.addEventListener("click", e => {
+//     e.preventDefault()
 
-    if (input.value.toString().length !== 0) {
-        getWeather(input.value.toString()).then(res => console.log(res))
-    }
-})
+//     if (input.value.toString().length !== 0) {
+//         getWeather(input.value.toString()).then(res => console.log(res))
+//     }
+// })
