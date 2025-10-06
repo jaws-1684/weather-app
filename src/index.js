01b6a28aebd4a60b22ec092fd4e1fefc81b8d694
dan <dis.131998@gmail.com>
2025-10-06 13:18:50 +0300

    add: form that will let users input their location and will fetch the weather info

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./styles.css";
 import { getWeather } from "./weather.js";
+
+const submitBtn = document.querySelector("#submit")
+const input = document.querySelector("input[name='location']")
+
+submitBtn.addEventListener("click", e => {
+    e.preventDefault()
+
+    if (input.value.toString().length !== 0) {
+        getWeather(input.value.toString()).then(res => console.log(res))
+    }
+})
