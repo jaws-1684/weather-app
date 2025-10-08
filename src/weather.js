55df33650ff254498fd40d724aa70fea4a7a0882
dan <dis.131998@gmail.com>
2025-10-08 12:30:30 +0300

    refactor: to using a class

--- src/weather.js
+++ src/weather.js
@@ -1,33 +1,37 @@
-function process(visualcrossingobj) {
-  let { visibility, conditions, humidity, sunrise, sunset, temp, datetime, feelslike, pressure, windspeed, icon } = visualcrossingobj 
-  return ({ visibility, 
-        conditions, 
-        humidity, 
-        sunrise, 
-        sunset, 
-        temp,
-        datetime,
-        feelslike,
-        sunrise,
-        pressure,
-        windspeed,
-        icon   
-      }) 
+class Weather {
+  static process(visualcrossingobj) {
+    let { visibility, conditions, humidity, sunrise, sunset, temp, datetime, feelslike, pressure, windspeed, icon } = visualcrossingobj 
+    return ({ visibility, 
+          conditions, 
+          humidity, 
+          sunrise, 
+          sunset, 
+          temp,
+          datetime,
+          feelslike,
+          sunrise,
+          pressure,
+          windspeed,
+          icon   
+        }) 
 
-}
-export function getWeather(param="london") {
-  return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${param}/next3days?unitGroup=metric&key=UVW6SQDY49YCTPXV9BBWS6PBJ`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(result) {
-      let daysArr = result.days
-      let res = []
-      daysArr.forEach(day => {
-        res.push(process(day))
+  }
+  static getWeather(param="london") {
+    return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${param}/next3days?unitGroup=metric&key=UVW6SQDY49YCTPXV9BBWS6PBJ`)
+      .then(function(response) {
+        return response.json();
       })
-      return res
-    })
+      .then(function(result) {
+        let daysArr = result.days
+        let res = []
+        daysArr.forEach(day => {
+          res.push(process(day))
+        })
+        return res
+      })
+  }
 }
+
+
 
 
