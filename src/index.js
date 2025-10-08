5049456f2870817c593cd0a6e817525be1336c43
dan <dis.131998@gmail.com>
2025-10-08 14:55:37 +0300

    add: favicon

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import "./styles.css";
 import { DOM } from "./DOM.js";
 import { App } from "./app.js";
 import { Weather } from "./weather.js";
+import favicon from "./icons/favicon.png"
 
-Weather.getWeather().then(res => {
-	App.main(res)
-})
+let link = document.querySelector("#favicon")
+link.href = favicon
+// Weather.getWeather().then(res => {
+// 	App.main(res)
+// })
 
-App.listen()
+// App.listen()
